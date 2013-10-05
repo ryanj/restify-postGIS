05eee9fed4b453df357718d702e0230a05cc732c
ryanj <ryan.jarvinen@gmail.com>
2013-10-04 19:17:20 -0700

    adding table alias t, returning result.rows

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.use(restify.fullResponse())
 
 // Routes
 app.get('/parks/within', function (req, res, next){
-  //points = [{"lat": 42.359999999999999, "gid": 54, "lon": -71.064539999999994, "name": "Boston African American National Historic Site"}, {"lat": 42.31861, "gid": 55, "lon": -70.945650000000001, "name": "Boston Harbor Islands National Recreation Area"}, {"lat": 42.359999999999999, "gid": 56, "lon": -71.056169999999995, "name": "Boston National Historical Park"}, {"lat": 42.325000000000003, "gid": 181, "lon": -71.132050000000007, "name": "Frederick Law Olmsted National Historic Site"}, {"lat": 42.345829999999999, "gid": 281, "lon": -71.124279999999999, "name": "John Fitzgerald Kennedy National Historic Site"}, {"lat": 42.376669999999997, "gid": 323, "lon": -71.126230000000007, "name": "Longfellow National Historic Site"}];
-  //res.send(points);
   //clean these variables:
   var query = req.query;
   var limit = (typeof(query.limit) !== "undefined") ? query.limit : 40;
@@ -34,15 +32,13 @@ app.get('/parks/within', function (req, res, next){
       res.send(500, {http_status:500,error_msg: err})
       return console.error('could not connect to postgres', err);
     }
-    pg.query('SELECT gid,name,ST_X(the_geom) as lon,ST_Y(the_geom) as lat FROM ' + table_name+ ' WHERE ST_Intersects( ST_MakeEnvelope('+query.lon1+", "+query.lat1+", "+query.lon2+", "+query.lat2+", 4326), t.the_geom) LIMIT "+limit+';', function(err, result) {
+    pg.query('SELECT gid,name,ST_X(the_geom) as lon,ST_Y(the_geom) as lat FROM ' + table_name+ ' t WHERE ST_Intersects( ST_MakeEnvelope('+query.lon1+", "+query.lat1+", "+query.lon2+", "+query.lat2+", 4326), t.the_geom) LIMIT "+limit+';', function(err, result) {
       if(err) {
         res.send(500, {http_status:500,error_msg: err})
         return console.error('error running query', err);
       }
-      //console.log(result.rows[0].theTime);
       console.dir(result);
-      //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-      res.send(result);
+      res.send(result.rows);
       pg.end();
     });
   });
@@ -53,15 +49,13 @@ app.get('/parks', function (req, res, next){
       res.send(500, {http_status:500,error_msg: err})
       return console.error('could not connect to postgres', err);
     }
-    pg.query('SELECT gid,name,ST_X(the_geom) as lon,ST_Y(the_geom) as lat FROM ' + table_name, function(err, result) {
+    pg.query('SELECT gid,name,ST_X(the_geom) as lon,ST_Y(the_geom) as lat FROM ' + table_name +';', function(err, result) {
       if(err) {
         res.send(500, {http_status:500,error_msg: err})
         return console.error('error running query', err);
       }
-      //console.log(result.rows[0].theTime);
       console.dir(result);
-      //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-      res.send(result);
+      res.send(result.rows);
       pg.end();
     });
   });
